0e4ea294b0b8ec42b6eeb4af2b6b65178209b66c
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-08-16 13:24:38 +0530

    Home Page Completed

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(window).scroll(function(){
 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
+
+$(function() {
+	$(document).on('click', 'a.page-scroll', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 600, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
