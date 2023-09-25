7b881dcb3cdfbe25cc59e8ad0dd26d45f0d32e02
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-09-25 11:23:13 +0530

    Styles updated and new features added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 $(window).scroll(function() {
-	if ($(document).scrollTop() > 50) {
+	if ($(document).scrollTop() > 20) {
 	  $('.navbar').addClass('color-change');
+	  $('.arrow').addClass('newarrow')
+	  $('.sWord').addClass('newWord')
 	} else {
 	  $('.navbar').removeClass('color-change');
+	  $('.arrow').removeClass('newarrow')
+	  $('.sWord').removeClass('newWord')
+	}
+  });
+
+
+  $(window).scroll(function() {
+	if ($(document).scrollTop() > 900) {
+	  $('.time').addClass('newTime');
+	} else {
+	  $('.time').removeClass('newTime');
 	}
   });
