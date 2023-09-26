0ec826f9731b959480c7bc87220a133fc21ea169
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-09-26 19:00:59 +0530

    JS Updated

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
 $(window).scroll(function() {
-	if ($(document).scrollTop() > 20) {
+	if ($(document).scrollTop() > 30) {
 	  $('.navbar').addClass('color-change');
-	  $('.arrow').addClass('newarrow')
-	  $('.sWord').addClass('newWord')
 	} else {
 	  $('.navbar').removeClass('color-change');
-	  $('.arrow').removeClass('newarrow')
-	  $('.sWord').removeClass('newWord')
 	}
-  });
+});
+
+  
+$(window).scroll(function() {
+	if ($(document).scrollTop() > 10) {
+		$('.arrow').addClass('newarrow')
+		$('.sWord').addClass('newWord')
+	} else {
+		$('.arrow').removeClass('newarrow')
+		$('.sWord').removeClass('newWord')
+	}
+});
 
 
-  $(window).scroll(function() {
+$(window).scroll(function() {
 	if ($(document).scrollTop() > 900) {
 	  $('.time').addClass('newTime');
 	} else {
 	  $('.time').removeClass('newTime');
 	}
-  });
+});
+
